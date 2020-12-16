419d802ba4c9ea5afd99c104da3d885dce552f78
hknuKWJ <62840997+hknuKWJ@users.noreply.github.com>
2020-12-16 14:55:05 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	
+var random = Math.floor(Math.random() * $('.topping1').length);
+$('.topping1').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping2').length);
+$('.topping2').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping3').length);
+$('.topping3').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping4').length);
+$('.topping4').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping5').length);
+$('.topping5').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping6').length);
+$('.topping6').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping7').length);
+$('.topping7').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping8').length);
+$('.topping8').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping9').length);
+$('.topping9').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping10').length);
+$('.topping10').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping11').length);
+$('.topping11').hide().eq(random).show();
+var random = Math.floor(Math.random() * $('.topping12').length);
+$('.topping12').hide().eq(random).show();
+
+})
